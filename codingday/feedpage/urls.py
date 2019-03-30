4c02695e18a3a14353346560a3bc00c97f98b0ec
HyunSangHan <gustkd3@gmail.com>
2019-03-30 17:51:38 +0900

    190330 Update: CRUD

--- codingday/feedpage/urls.py
+++ codingday/feedpage/urls.py
@@ -3,4 +3,7 @@ from feedpage import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/', views.new, name='new'), # 추가
+    path('<int:id>/', views.show, name='show'), # 추가
+    path('<int:id>/delete', views.delete, name='delete') #추가
 ]
