139666f1bfe70b072ba639bc82a0e5038d60d342
HyunSangHan <gustkd3@gmail.com>
2019-03-30 16:35:57 +0900

    190330 Make: feed page structure

--- codingday/feedpage/views.py
+++ codingday/feedpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Feed # 추가. (참고: .models == feeds.models)
 
-# Create your views here.
-def index(request) :
-    return render(request, 'feedpage/index.html')
+def index(request): # 원래 있던 index 함수 수정.
+    feeds = Feed.objects.all()
+    return render(request, 'feedpage/index.html', {'feeds': feeds})
