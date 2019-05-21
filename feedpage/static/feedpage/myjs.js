eda0330969d6d1990ba8ec54fcfe61c1fceb04b2
HyunSangHan <gustkd3@gmail.com>
2019-05-21 13:38:45 +0900

    190521 Done: more info box structure by jquery

--- feedpage/static/feedpage/myjs.js
+++ feedpage/static/feedpage/myjs.js
@@ -3,4 +3,12 @@ $(document).ready(() => {
         $('.all-comments').slideDown();
         $('.toggle-comments').hide();
     })
+    $('.mdl-js-button').on('click', () => {
+        $('.more-info').fadeToggle();
+        $('.transp-bg').toggle();
+    })
+    $('.transp-bg').on('click', () => {
+        $('.more-info').hide();
+        $('.transp-bg').hide();
+    })
 })
