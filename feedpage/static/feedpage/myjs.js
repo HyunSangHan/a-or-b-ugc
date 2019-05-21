af6ac9807d4aa4f8c0601bf1ac7381aff84e6d69
HyunSangHan <gustkd3@gmail.com>
2019-05-21 21:02:48 +0900

    190521 Edit: jQuery for applying each element

--- feedpage/static/feedpage/myjs.js
+++ feedpage/static/feedpage/myjs.js
@@ -1,14 +1,9 @@
 $(document).ready(() => {
-    $('.toggle-comments').on('click', () => {
-        $('.all-comments').slideDown();
-        $('.toggle-comments').hide();
+    $('.toggle-comments').on('click', function() {
+        $(this).next().slideDown();
+        $(this).hide();
     })
-    $('.mdl-js-button').on('click', () => {
-        $('.more-info').fadeToggle();
-        $('.transp-bg').toggle();
-    })
-    $('.transp-bg').on('click', () => {
-        $('.more-info').hide();
-        $('.transp-bg').hide();
+    $('.mdl-js-button').on('click', function() {
+        $(this).next().toggle();
     })
 })
