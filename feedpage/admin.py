a88f9ddf566c4685d52b949b788bdfc4e7370b06
HyunSangHan <gustkd3@gmail.com>
2019-08-04 20:48:19 +0900

    190804 Fixes #22 issue

--- feedpage/admin.py
+++ feedpage/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Feed, FeedComment, Report, HashTag, TagRelation, Upvote
+from .models import Feed, FeedComment, Report, HashTag, TagRelation, Upvote, CommentUpvote
 # Register your models here.
 
 admin.site.register(Feed)
+admin.site.register(Upvote)
 admin.site.register(FeedComment)
+admin.site.register(CommentUpvote)
 admin.site.register(Report)
 admin.site.register(HashTag)
-admin.site.register(Upvote)
 admin.site.register(TagRelation)
